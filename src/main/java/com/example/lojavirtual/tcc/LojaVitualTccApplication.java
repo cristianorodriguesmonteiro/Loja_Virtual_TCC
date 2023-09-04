1b63719684fdf227c70f6214f8d1edc527e450a5
Cristiano Rodrigues <crmprojetosweb@gmail.com>
2023-09-04 15:07:09 -0300

    commit inicial projeto tcc

---
+++ src/main/java/com/example/lojavirtual/tcc/LojaVitualTccApplication.java
@@ -0,0 +1,13 @@
+package com.example.lojavirtual.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVitualTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVitualTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lojavirtual/tcc/LojaVitualTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lojavirtual.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVitualTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
